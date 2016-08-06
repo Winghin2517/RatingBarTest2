2b2b4df61dd0443f06c552c6dfa9e7d2edafd669
Winghin2517 <simon.chan2014@gmail.com>
2016-08-06 19:13:32 +0200

    added mutate method to drawablecompat

--- app/src/main/java/test/com/ratingbartest2/MainActivity.java
+++ app/src/main/java/test/com/ratingbartest2/MainActivity.java
@@ -1,6 +1,9 @@
 package test.com.ratingbartest2;
 
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RatingBar;
 
@@ -11,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingbar);
-/*        LayerDrawable layerDrawable2 = (LayerDrawable) ratingBar.getProgressDrawable();
-        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable2.getDrawable(0)),
+        LayerDrawable layerDrawable2 = (LayerDrawable) ratingBar.getProgressDrawable();
+        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable2.getDrawable(0).mutate()),
                 ContextCompat.getColor(getApplicationContext(), android.R.color.background_dark));
-        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable2.getDrawable(1)),
+        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable2.getDrawable(1).mutate()),
                 ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)); // Partial star
-        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable2.getDrawable(2)),
-                ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));*/
+        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable2.getDrawable(2).mutate()),
+                ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
         ratingBar.setIsIndicator(false);
         ratingBar.setRating(3.6f);
         ratingBar.setStepSize(0.1f);
